f14290d07a7dce692ff6196e85dd8f46e6c64583
Godpu <908662421@qq.com>
2021-01-24 02:12:35 +0800

    update: dblclick handler some code

--- src/client.js
+++ src/client.js
@@ -6,36 +6,44 @@
  * @param line {number}
  * @param content {string}
  * */
-function pullRequest(
-	sourcePath,
-	line,
-	content
-) {
-	// TODO
+function pullRequest(sourcePath, line, content) {
+  // TODO
+  console.log("发起PR===>", sourcePath, line, content);
 }
 
-module.exports = ({Vue}) => {
-	return {
-		name: 'vuepress-plugin-editable',
-		content: Vue.mixin({
-			mounted() {
-				// TODO: 为什么这里会执行这么多次呢？
-				console.info(1);
-				const targetNode = document.querySelector('body');
-				const dblClick = (event) => {
-					const repoPrefix = this.$themeConfig.repo || '';
-					if (!repoPrefix || !repoPrefix.length) {
-						console.warn('Warning: You have not set the repo url');
-					}
-					const fullRemoteUrl = repoPrefix + this.$page.relativePath;
-					console.log('TODO pull pr to==>', fullRemoteUrl);
-				};
-				
-				if (targetNode) {
-					targetNode.removeEventListener('dblclick', dblClick);
-					targetNode.addEventListener('dblclick', dblClick);
-				}
-			}
-		})
-	};
+export default {
+  mounted() {
+    const targetNode = document.querySelector("body");
+    const dblClick = (event) => {
+      console.log("event=>");
+      const editableLine = event.target.getAttribute("data-editable-line");
+      if (editableLine || editableLine != null) {
+        const repoPrefix = this.$themeConfig.repo || "";
+        if (!repoPrefix || !repoPrefix.length) {
+          console.warn("Warning: You have not set the repo url");
+        }
+        const sourcePath = repoPrefix + this.$page.relativePath;
+        console.log("TODO pull pr to==>", sourcePath);
+        const content = event.target.innerHTML;
+
+        const isEditable = event.target.getAttribute("contenteditable");
+        if (!isEditable) {
+          event.target.setAttribute("contenteditable", true);
+        } else event.target.setAttribute("contenteditable", "");
+        // todo
+        pullRequest(sourcePath, editableLine, content);
+
+        // TODO 这里的content 或许需要转换为 markdown 处理
+      }
+    };
+
+    if (targetNode) {
+      targetNode.removeEventListener("dblclick", dblClick);
+      targetNode.addEventListener("dblclick", dblClick);
+    }
+
+    // TODO: 移除圈外则恢复原始，
+    // TODO 再通过按钮设置发起 PR
+    // TODO: 存储原始内容
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
-const {resolve} = require('path');
+const { resolve } = require("path");
 module.exports = (options, ctx) => {
-	return {
-		name: "vuepress-plugin-editable",
-		multiple: true,
-		// 钩子在应用初始化之后，并在某些特定的函数式 API 执行之前执行
-		// 这些函数式 API 包括： clientDynamicModules enhanceAppFiles
-		// async ready(...args) {
-		//   console.log("ready==>", args);
-		// },
-		// updated(...args) {
-		//   console.log("updated==>", args);
-		// },
-		async generated(pagePaths) {
-			console.log("pagePaths==>", pagePaths);
-		},
-		
-		extendMarkdown(md) {
-			// console.info('==>', resolve(__dirname, 'client.js'));
-			md.use(require("./line"), ctx);
-		},
-		extendPageData($page) {
-			const {
-				_context,
-				_filePath,
-			} = $page;
-			const cwdLen = _context.cwd.length;
-			$page.remoteRelativePath = (_filePath.substr(cwdLen)).replace(/\\/g, '/');
-		},
-		// enhanceAppFiles: require('./client.js')
-		enhanceAppFiles: resolve(__dirname, 'client.js')
-	};
-};
+  return {
+    name: "vuepress-plugin-editable",
+    multiple: true,
+    // 钩子在应用初始化之后，并在某些特定的函数式 API 执行之前执行
+    // 这些函数式 API 包括： clientDynamicModules enhanceAppFiles
+    // async ready(...args) {
+    //   console.log("ready==>", args);
+    // },
+    // updated(...args) {
+    //   console.log("updated==>", args);
+    // },
+    async generated(pagePaths) {
+      console.log("pagePaths==>", pagePaths);
+    },
 
-// themeConfig.repo  or $themeConfig.repo
+    extendMarkdown(md) {
+      // console.info('==>', resolve(__dirname, 'client.js'));
+      md.use(require("./line"), ctx);
+    },
+    extendPageData($page) {
+      const { _context, _filePath = "" } = $page;
+      const cwdLen = _context.cwd.length;
+      $page.remoteRelativePath = _filePath.substr(cwdLen).replace(/\\/g, "/");
+    },
+    // enhanceAppFiles: require('./client.js')
+    clientRootMixin: resolve(__dirname, "client.js"),
+  };
+};
