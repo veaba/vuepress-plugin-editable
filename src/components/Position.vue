9915fa5c9f76553a3d5d168957b75f556837580a
veaba <908662421@qq.com>
2021-02-09 19:50:46 +0800

    feat: add line count

---
+++ src/components/Position.vue
@@ -0,0 +1,28 @@
+<!--
+文件行数 定位
+-->
+<template>
+  <div class="editable-lines">
+    <span v-for="item in lines" :key="item">{{ item }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    lines: {
+      type: Number,
+    },
+  },
+};
+</script>
+<style scoped>
+.editable-lines {
+  /* width: 80px; */
+  background-color: #fff;
+  padding: 130px 0 0 15px;
+}
+.editable-lines > span {
+  display: block;
+}
+</style>
